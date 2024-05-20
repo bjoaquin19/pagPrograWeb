3e753e3244f15ee5b3b42d90155f98de7ffb6f80
unknown <CETECOM@AOAWSB03LC1107.duoc.acad>
2024-05-20 12:11:51 -0400

    casi la cago wuajajajajajaja

---
+++ PagUmbrellaCorpa/js/alooo.js
@@ -0,0 +1,51 @@
+
+// $(document).ready(function(){
+
+//     $.get('http://fakestoreapi.com/products', function(data){
+//         $.each(data, function(i, item){
+//             html = `
+//             <div class="card" style="width: 18rem;">
+//                 <img src="${item.image}" width=300 height=300 class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">
+//                         ${item.title}
+//                     </h5>
+//                     <h6 class="card-title">
+//                         ${item.category}
+//                     </h6>
+//                     <p class="card-text">
+//                     ${item.description}
+//                     </p>
+//                     <a href="#" class="btn btn-primary">Buscar en Amazon</a>
+//                 </div>
+//             </div>
+//             `;
+//             $("#recuadro").append(html);
+//         });
+//     });
+// });
+$(document).ready(function(){
+
+    $.get('http://fakestoreapi.com/products', function(data){
+        $.each(data.slice(0, 6), function(i, item){
+            html = `
+            <div class="card" style="width: 18rem;">
+                <img src="${item.image}" width=300 height=300 class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        ${item.title}
+                    </h5>
+                    <h6 class="card-title">
+                        ${item.category}
+                    </h6>
+                    <p class="card-text">
+                    ${item.description}
+                    </p>
+                    <a href="#" class="btn btn-primary">Buscar en Amazon</a>
+                </div>
+            </div>
+            `;
+            $("#recuadro").append(html);
+        });
+    });
+});
